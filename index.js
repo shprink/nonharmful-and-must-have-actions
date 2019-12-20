619efab5f6d5e1ff60f117bdee8a681bf7696421
Julien Renaux <julien.renaux@gmail.com>
2019-12-20 12:01:14 +0100

    using the action v1

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 
 try {
   const mySecret = core.getInput("my-secret");
-  console.log(`Hello ${mySecret}!`);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log(`DO SOMETHING REALLY COOL WITH THE SECRET FOR YEARS`);
 } catch (error) {
   core.setFailed(error.message);
 }
